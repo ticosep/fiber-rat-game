8e19eb3a9369177803d363730ead9a1514103bed
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2023-03-15 16:06:39 -0300

    wip: add spin z axis to spinningWheel

--- src/levels/Level1/components/SpinningWheel.tsx
+++ src/levels/Level1/components/SpinningWheel.tsx
@@ -1,18 +1,25 @@
+import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { MeshRef } from "../../../types";
 
 export function SpinningWheel() {
   const ref = useRef<MeshRef>(null);
 
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.z += delta;
+    }
+  });
+
   return (
     <mesh ref={ref} position={[-2, 0, 0]} rotation={[0, -1, 0]}>
       <mesh key="first-ring" ref={ref} position={[0, 0, 0]}>
         <meshStandardMaterial color="orange" />
-        <ringGeometry args={[2, 2.5, 16]} />
+        <ringGeometry args={[2, 2.5, 8]} />
       </mesh>
       <mesh key="second-ring" ref={ref} position={[0, 0, -2]}>
         <meshStandardMaterial color="hotpink" />
-        <ringGeometry args={[2, 2.5, 16]} />
+        <ringGeometry args={[2, 2.5, 8]} />
       </mesh>
     </mesh>
   );
